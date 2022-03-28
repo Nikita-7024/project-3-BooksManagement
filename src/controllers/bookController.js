f8af5ebcc6f1c4fcff2b1ad9bf234e7e7372a8e6
Nikita-7024 <nikita.singh7024@gmail.com>
2022-03-29 02:55:34 +0530

    Project-3 Book Api

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -92,57 +92,168 @@ const createBook = async function (req, res) {
 
 
 
-const listBook = async function (req, res) {
-
+const getBook = async function (req, res) {
     try {
-        const isValidRequestBody = function (requestBody) {
-            return Object.keys(requestBody).length > 0
-        }
-
-        const filterQuery = { isDeleted: false, deletedAt: null }
-        const queryParams = req.query
-
-        if (isValidRequestBody(queryParams)) {
-            const { title, excerpt, userId, category, releasedAt, reviews } = queryParams
-
-            if (isValid(userId) && isValidObjectId(userId)) {
-                filterQuery['userId'] = userId
+        // filter data by query params and sorting by title----------------------------------------------
+        let filter = req.query;
+        if (Object.keys(filter).length === 0) {
+            let book = await BookModel.find({ isDeleted: false })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Books not found." })
             }
-            if (isValid(title)) {
-                filterQuery['title'] = title.trim()
-            }
-            if (isValid(excerpt)) {
-                filterQuery['excerpt'] = excerpt.trim()
-            }
-            if (isValid(category)) {
-                filterQuery['category'] = category.trim()
-            }
-            if (isValid(releasedAt)) {
-                filterQuery['releasedAt'] = reviews.trim()
-            }
-            if (isValid(reviews)) {
-                filterQuery['reviews'] = releasedAt.trim()
-            }
-
+            let bookData = book.length;
+            return res.status(200).send({ status: true, total: bookData, data: book })
 
         }
-
-        const book = await BookModel.find(queryParams)
-        if (!book) {
-            return res.status(400).send({ status: false, message: 'Book not found' })
+        // query params empty----------------
+        if (filter.userId == undefined && filter.category == undefined && filter.subcategory == undefined) {
+            let book = await BookModel.find({ isDeleted: false })
+            if (!book) {
+                return res.status(404).send({ status: false, msg: "Book not found." })
+            }
+            return res.status(200).send({ status: true, msg: "Success", data: book })
         }
-        else {
-            return res.status(200).send({ status: true, message: 'Book List', data: book })
+        //    only userId in params----------
+        if (filter.userId != undefined && filter.category == undefined && filter.subcategory == undefined) {
+            let id = filter.userId
+
+            let book = await BookModel.find({ $and: [{ userId: { $in: [id] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with this uderId." })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
+        }
+        //  only category in params-----------
+        if (filter.userId == undefined && filter.category != undefined && filter.subcategory == undefined) {
+            let category = filter.category
+
+            let book = await BookModel.find({ $and: [{ category: { $in: [category] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with this category" })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
+        }
+        //   only subcategory in params ------------
+        if (filter.userId == undefined && filter.category == undefined && filter.subcategory != undefined) {
+            let subCat = filter.subcategory
+            let book = await BookModel.find({ $and: [{ subcategory: { $in: [subCat] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with this subcategory" })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
+        }
+        //  both category and subcategory in params----------
+        if (filter.userId == undefined && filter.category != undefined && filter.subcategory != undefined) {
+            let subCat = filter.subcategory
+            let category = filter.category
+            let book = await BookModel.find({ $and: [{ subcategory: { $in: [subCat] } }, { category: { $in: [category] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with category and subcategory" })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
+        }
+        //   both userId and category in params---------
+        if (filter.userId != undefined && filter.category != undefined && filter.subcategory == undefined) {
+
+            let category = filter.category
+            let id = filter.userId
+            let book = await BookModel.find({ $and: [{ category: { $in: [category] } }, { userId: { $in: [id] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with userId and category" })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
+        }
+        //  both userId and subcategory in params--------
+        if (filter.userId != undefined && filter.category == undefined && filter.subcategory != undefined) {
+            let subCat = filter.subcategory
+            let id = filter.userId
+            let book = await BookModel.find({ $and: [{ subcategory: { $in: [subCat] } }, { userId: { $in: [id] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with userId and subcategory" })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
+        }
+        // all userId, category, subcategory in params----------
+        if (filter.category != undefined && filter.subcategory != undefined && filter.userId != undefined) {
+            let subCat = filter.subcategory
+            let category = filter.category
+            let id = filter.userId
+            let book = await bookModel.find({ $and: [{ subcategory: { $in: [subCat] } }, { category: { $in: [category] } }, { userId: { $in: [id] } }, filter, { isDeleted: false }] }).sort({ "title": 1 }).select({
+                _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, subcategory: 1
+            })
+            if (book.length === 0) {
+                return res.status(404).send({ status: false, msg: "Book not found with userId,category, subcategory" })
+            }
+            return res.status(200).send({ status: true, mesg: "Success", data: book })
         }
 
-
+    } catch (error) {
+        return res.status(500).send({ msg: "Error", error: error.message })
     }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ status: "failed", message: err.message })
+}
+
+
+
+const updateBook = async function (req, res) {
+    try {
+        let bookId = req.params.bookId;
+        let body = req.body;
+        if (Object.keys(body).length === 0) {
+            return res.status(400).send({ status: false, msg: "Enter Data to update." })
+        }
+        let validBook = await BookModel.findOne({ $and: [{ _id: bookId }, { isDeleted: false }] })
+        if (!validBook) {
+            return res.status(404).send({ status: false, msg: "Book not found" })
+        }
+
+        let updatedata = req.body;
+        let updatedBook = await BookModel.findOneAndUpdate({ _id: bookId }, { title: updatedata.title, excerpt: updatedata.excerpt, releasedAt: updatedata.releasedAt, ISBN: updatedata.ISBN }, { new: true, upsert: true });
+     
+        return res.status(201).send({ status: true, message: "Success", data: updatedBook })
+
+    } catch (err) {
+        return res.status(500).send({ Error: err.message })
+    }
+
+}
+
+
+
+
+
+const deleteBookById = async function (req, res) {
+    try {
+        let book = req.params.bookId;
+
+        let validBook = await BookModel.findOneAndUpdate(
+            { _id: book, isDeleted: false },
+            { $set: { isDeleted: true, deletedAt: Date.now() } },
+            { new: true }
+        )
+        if (!validBook) {
+            return res.status(404).send({ status: false, msg: "No such book exists" })
+        }
+        return res.status(200).send({ status: true, msg: "Book deleted" })
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, msg: "Error", error: error.message })
     }
 
 }
 
 module.exports.createBook = createBook
-module.exports.listBook = listBook
+module.exports.getBook = getBook
+module.exports.updateBook = updateBook
+module.exports.deleteBookById = deleteBookById
--- src/routes/route.js
+++ src/routes/route.js
@@ -15,7 +15,20 @@ router.post("/user/login",UserController.loginUser)
 // create Book.......
 router.post("/book/create",BookController.createBook)
 
+
 // get book list.....
-router.get("/book/get",BookController.listBook)
+router.get("/book/get",BookController.getBook)
+
+
+// update Book by Id.....
+router.post("/update/books/:bookId",BookController.updateBook)
+
+
+// delete book by id....
+router.delete("/delete/books/:bookId",BookController.deleteBookById )
+
+
+
+
 
 module.exports = router;
